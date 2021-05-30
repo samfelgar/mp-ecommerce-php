dc970ff6f36fb74c0d6f115c8b22b8194c2d8d17
Samuel Felipe <samfelgar@gmail.com>
2021-05-30 19:14:29 -0300

    Deal with notifications

--- app/Http/Controllers/PaymentNotificationController.php
+++ app/Http/Controllers/PaymentNotificationController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaymentNotificationController extends Controller
 {
     public function feedback(Request $request)
     {
+        $data = $request->json();
 
+        if (!empty($data)) {
+            Log::info(print_r($data, true));
+        }
+
+        return response(null, 200);
     }
 }
